1f55d4797acaab2560355cc7fdd8532417d2b3fe
Jennifer Birsan <jennibirsan@Cornels-MBP.attlocal.net>
2019-10-04 00:17:36 -0500

    uninstall ngrok

--- server/node/server.js
+++ server/node/server.js
@@ -13,7 +13,7 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
-const ngrok = config.ngrok.enabled ? require('ngrok') : null;
+// const ngrok = config.ngrok.enabled ? require('ngrok') : null;
 const app = express();
 
 // Setup useful middleware.
